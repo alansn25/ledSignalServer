43a190f82584f925c44369386d7caf11411fca9e
Alan Nogueira <alansn@gmail.com>
2018-06-22 17:38:13 -0300

    inital 4

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
+const _ = require('lodash');
+const yargs = require('yargs');
 const express = require('express');
+
+const port = process.env.PORT || 3000;
+
 const http = require('http');
 const path = require('path');
 const socketIO = require('socket.io');
@@ -6,10 +11,14 @@ const socketIO = require('socket.io');
 
 
 const publicPath = path.join(__dirname,'../public' );
-const port = process.env.PORT || 3000;
+
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
 
 
-hhdhd
+
+
+app.listen(port, () =>{
+    console.log(`Server is up on port ${port}`);
+})
